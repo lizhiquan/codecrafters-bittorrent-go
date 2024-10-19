41e1218d87db66e6a525f87ecc6c47533d02b217
Vincent Le <quyen.lechi@gmail.com>
2024-10-19 15:24:49 -0700

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -4,53 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
-	"unicode"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
+	"strings"
+
+	bencode "github.com/jackpal/bencode-go"
 )
-
-// Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
-var _ = json.Marshal
-
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		return decodeString(bencodedString)
-	} else if bencodedString[0] == 'i' {
-		return decodeInteger(bencodedString)
-	}
-	return "", fmt.Errorf("Unsupported bencode type")
-}
-
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeString(bencodedString string) (interface{}, error) {
-	var firstColonIndex int
-
-	for i := 0; i < len(bencodedString); i++ {
-		if bencodedString[i] == ':' {
-			firstColonIndex = i
-			break
-		}
-	}
-
-	lengthStr := bencodedString[:firstColonIndex]
-
-	length, err := strconv.Atoi(lengthStr)
-	if err != nil {
-		return "", err
-	}
-
-	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-}
-
-func decodeInteger(bencodedString string) (interface{}, error) {
-	if bencodedString[len(bencodedString)-1] != 'e' {
-		return nil, fmt.Errorf("Invalid integer bencode")
-	}
-
-	return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-}
 
 func main() {
 	command := os.Args[1]
@@ -58,7 +15,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, err := bencode.Decode(strings.NewReader(bencodedValue))
 		if err != nil {
 			fmt.Println(err)
 			return
