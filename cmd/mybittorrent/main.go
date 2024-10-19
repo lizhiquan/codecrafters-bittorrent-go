3cbf8d6af7fca808c25f08c36e9f88e7d66550a2
Vincent Le <quyen.lechi@gmail.com>
2024-10-19 15:38:34 -0700

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -9,20 +9,58 @@ import (
 	bencode "github.com/jackpal/bencode-go"
 )
 
+func cmdDecode() {
+	bencodedValue := os.Args[2]
+
+	decoded, err := bencode.Decode(strings.NewReader(bencodedValue))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	jsonOutput, _ := json.Marshal(decoded)
+	fmt.Println(string(jsonOutput))
+}
+
+func cmdInfo() {
+	torrentFile, err := os.Open(os.Args[2])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer torrentFile.Close()
+
+	type TorrentInfo struct {
+		Length      int    `bencode:"length"`
+		Name        string `bencode:"name"`
+		PieceLength int    `bencode:"piece length"`
+		Pieces      string `bencode:"pieces"`
+	}
+
+	type Torrent struct {
+		Announce string      `bencode:"announce"`
+		Info     TorrentInfo `bencode:"info"`
+	}
+
+	var torrent Torrent
+	err = bencode.Unmarshal(torrentFile, &torrent)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Tracker URL: %s\n", torrent.Announce)
+	fmt.Printf("Length: %d\n", torrent.Info.Length)
+}
+
 func main() {
 	command := os.Args[1]
 
 	if command == "decode" {
-		bencodedValue := os.Args[2]
-
-		decoded, err := bencode.Decode(strings.NewReader(bencodedValue))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		jsonOutput, _ := json.Marshal(decoded)
-		fmt.Println(string(jsonOutput))
+		cmdDecode()
+	} else if command == "info" {
+		cmdInfo()
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
